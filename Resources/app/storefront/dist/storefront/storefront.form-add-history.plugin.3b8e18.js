034070059599ca04563fce188e753c2222c18f0d
shopwareBot <shopwarebot@shopware.com>
2025-02-14 11:22:23 +0000

    repo-split-20250214-1143 (+ assets)

---
+++ Resources/app/storefront/dist/storefront/storefront.form-add-history.plugin.3b8e18.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[87095],{7095:(s,t,i)=>{i.r(t),i.d(t,{default:()=>h});var e=i(9568);class h extends e.Z{static #s=this.options={entries:[]};init(){this.el.addEventListener("submit",this.pushHistoryEntries.bind(this))}pushHistoryEntries(){this.options.entries.forEach(s=>{let{state:t={},title:i,url:e}=s;history.pushState(t,i,e)})}}}}]);
